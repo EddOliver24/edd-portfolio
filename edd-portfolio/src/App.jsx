0166a0c8333379168129dc9c2d40941ac0df0f25
Edd Oliver E. Verzosa <edd.oliver.verzosa@gmail.com>
2024-07-27 19:46:19 +0800

    update: added technology logo and brief description of projects

--- edd-portfolio/src/App.jsx
+++ edd-portfolio/src/App.jsx
@@ -16,6 +16,11 @@ import alignAppImage from "./assets/img/align-mern-app.png";
 import lendingAppImage from "./assets/img/lending-mern-app.png";
 import pokidexAppImage from "./assets/img/pokidex-app.png";
 import todoAppImage from "./assets/img/todo-list-app.png";
+import mongoDbLogo from "./assets/img/mongodb-original.svg";
+import expressLogo from "./assets/img/express-original.svg";
+import reactLogo from "./assets/img/react-original.svg";
+import nodeLogo from "./assets/img/nodejs-original.svg";
+import jsLogo from "./assets/img/javascript-original.svg";
 import { useState } from "react";
 import EmailForm from "./components/EmailForm";
 import "./App.css";
@@ -29,7 +34,7 @@ function App() {
     <>
       <div>
         <section className="main-navigation fixed top-0 left-0 w-full z-50">
-          <header className="navigation">
+          <header className="navigation bg-secondary-300 text-secondary-500">
             <section className="navigation-logo">
               <img src={logoImage} alt="edd's logo" className="logo" />
             </section>
@@ -39,19 +44,29 @@ function App() {
             <nav className={`navigation-links ${menuOpen ? "open" : ""}`}>
               {" "}
               <a href="#portfolio">
-                <h3 className="inline-display font-size-nav">PORTFOLIO</h3>
+                <h3 className="inline-display font-size-nav text-secondary-500 font-bold">
+                  PORTFOLIO
+                </h3>
               </a>
               <a href="#about-me">
-                <h3 className="inline-display font-size-nav">ABOUT ME</h3>
+                <h3 className="inline-display font-size-nav text-secondary-500 font-bold">
+                  ABOUT ME
+                </h3>
               </a>
               <a href="#projects">
-                <h3 className="inline-display font-size-nav">PROJECTS</h3>
+                <h3 className="inline-display font-size-nav text-secondary-500 font-bold">
+                  PROJECTS
+                </h3>
               </a>
               <a href="#questions">
-                <h3 className="inline-display font-size-nav">CONTACTS</h3>
+                <h3 className="inline-display font-size-nav text-secondary-500 font-bold">
+                  CONTACTS
+                </h3>
               </a>
               <a href="https://eoverzosa.hashnode.dev/transitioning-from-engineer-to-full-stack-web-developer-a-journey-of-transformation">
-                <h3 className="inline-display font-size-nav">BLOG</h3>
+                <h3 className="inline-display font-size-nav text-secondary-500 font-bold">
+                  BLOG
+                </h3>
               </a>
             </nav>
           </header>
@@ -70,25 +85,32 @@ function App() {
         </section>
         <div className="partition" id="about-me"></div>
         <section>
-          <div className="about-me">
-            <h1 className="section-name padding">About Me</h1>
-            <p className="section-padding font-size-about">
-              A full-stack web developer with over 11 years of experience as an
-              Electrical Engineer, I bring a robust background in low voltage to
-              high voltage power supply systems across various sectors including
-              commercial, industrial, infrastructure, and oil and gas power
-              plants. My expertise spans testing and commissioning, maintenance,
-              and construction within these complex environments. Now, I'm
-              channeling my analytical skills and problem-solving mindset into
-              mastering web development to expand my skill set and contribute to
-              the digital landscape.
+          <div className="about-me bg-secondary-300">
+            <h1 className="section-name padding text-secondary-500">
+              About Me
+            </h1>
+            <p className="section-padding font-size-about text-secondary-500 font-bold">
+              As a full-stack web developer transitioning from a career as an
+              Electrical Engineer with over 11 years of experience, I bring a
+              wealth of technical expertise and problem-solving expertise to the
+              digital realm. My extensive background in low to high voltage
+              power supply systems within diverse sectors such as commercial,
+              industrial, infrastructure, and oil and gas power plants has honed
+              my skills in testing, commissioning, maintenance, and
+              construction. This technical foundation equips me with a unique
+              problem-solving mindset that I am now applying to web development.
+              I am eager to leverage my comprehensive engineering experience and
+              innovative approach to contribute meaningfully to the digital
+              landscape and drive impactful projects.
             </p>
             <div className="container-hire-resume">
               <a href="mailto:edd.oliver.verzosa@gmail.com">
                 <h2 className="color-fill">Hire Me</h2>
               </a>
-              <a href="/EDD OLIVER VERZOSA_WEBDEV_RESUME_2024.pdf" download>
-                <h2 className="border-box">Download Resume</h2>
+              <a href="/EDD OLIVER VERZOSA_WEBDEV_RESUME_07262024.pdf" download>
+                <h2 className="border-box bg-secondary-500 mb-4">
+                  Download Resume
+                </h2>
               </a>
             </div>
           </div>
@@ -148,7 +170,7 @@ function App() {
           <div className="partition" id="projects"></div>
         </section>
         <section>
-          <div className="projects">
+          <div className="projects bg-secondary-300 text-secondary-500">
             <h1 className="section-name padding projects-main">PROJECTS</h1>
             <section className="project-distribution mb-10">
               <img
@@ -156,9 +178,43 @@ function App() {
                 alt="align mern app logo"
                 className="project-logo"
               />
-              <h2 className="project-padding font-size-project -mb-6 mt-2">
+              <h2 className="project-padding font-size-project -mb-6 mt-2 font-bold">
                 Align App - Event Management System
               </h2>
+              <p className="text-justify w-4/5 mx-auto lg:text-lg lg:w-11/12 mb-2">
+                This application allows users to create and manage events
+                designed to foster community engagement. By providing tools for
+                event organization and promotion, it helps bring people together
+                for various social, educational, and recreational activities.
+              </p>
+
+              <div className="flex justify-center items-center space-x-6 my-4">
+                <img
+                  src={mongoDbLogo}
+                  alt="MongoDB Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={expressLogo}
+                  alt="Express JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={jsLogo}
+                  alt="JavaScript Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={reactLogo}
+                  alt="React JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={nodeLogo}
+                  alt="Node JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+              </div>
               <div className="button-group flex space-x-8 md:space-x-32 mt-2">
                 <a
                   href="https://align-mern-app.vercel.app/"
@@ -172,7 +228,7 @@ function App() {
                   href="https://github.com/EddOliver24/align-mern-app"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110"
+                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110 bg-secondary-500"
                 >
                   View Code
                 </a>
@@ -184,9 +240,42 @@ function App() {
                 alt="lending mern app logo"
                 className="project-logo"
               />
-              <h2 className="project-padding font-size-project -mb-6 mt-2">
+              <h2 className="project-padding font-size-project -mb-6 mt-2 font-bold">
                 Micro-Scale Lending App
               </h2>
+              <p className="text-justify w-4/5 mx-auto lg:text-lg lg:w-11/12 mb-2">
+                This app showcase a features for micro-scale lending business.
+                The user are limited to lenders. The user are able to login,
+                signup, and logout, check and add borrower, update payment of
+                borrower.
+              </p>
+              <div className="flex justify-center items-center space-x-6 my-4">
+                <img
+                  src={mongoDbLogo}
+                  alt="MongoDB Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={expressLogo}
+                  alt="Express JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={jsLogo}
+                  alt="JavaScript Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={reactLogo}
+                  alt="React JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={nodeLogo}
+                  alt="Node JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+              </div>
               <div className="button-group flex space-x-8 md:space-x-32 mt-2">
                 <a
                   href="https://lending-app-sandy.vercel.app/"
@@ -200,7 +289,7 @@ function App() {
                   href="https://github.com/EddOliver24/lending-app"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110"
+                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110 bg-secondary-500"
                 >
                   View Code
                 </a>
@@ -212,9 +301,27 @@ function App() {
                 alt="pokidex app logo"
                 className="project-logo"
               />
-              <h2 className="project-padding font-size-project -mb-6 mt-2">
+              <h2 className="project-padding font-size-project -mb-6 mt-2 font-bold">
                 Simple Pokidex App
               </h2>
+              <p className="text-justify w-4/5 mx-auto lg:text-lg lg:w-11/12 mb-2">
+                The Simple Pokédex app is a React-based web application that
+                allows users to explore and display information about Pokémon.
+                The app leverages the PokéAPI to fetch data and display it in a
+                user-friendly interface.
+              </p>
+              <div className="flex justify-center items-center space-x-6 my-4">
+                <img
+                  src={jsLogo}
+                  alt="JavaScript Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={reactLogo}
+                  alt="React JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+              </div>
               <div className="button-group flex space-x-8 md:space-x-32 mt-2">
                 <a
                   href="https://pokidex-app.vercel.app/"
@@ -228,7 +335,7 @@ function App() {
                   href="https://github.com/EddOliver24/pokidex-app"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110"
+                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110 bg-secondary-500"
                 >
                   View Code
                 </a>
@@ -240,9 +347,26 @@ function App() {
                 alt="todo app logo"
                 className="project-logo"
               />
-              <h2 className="project-padding font-size-project -mb-6 mt-2">
+              <h2 className="project-padding font-size-project -mb-6 mt-2 font-bold">
                 Todo App
               </h2>
+              <p className="text-justify w-4/5 mx-auto lg:text-lg lg:w-11/12 mb-2">
+                The Todo App is a simple and intuitive task management
+                application built with React.js. It allows users to add, manage,
+                and remove tasks with ease.
+              </p>
+              <div className="flex justify-center items-center space-x-6 my-4">
+                <img
+                  src={jsLogo}
+                  alt="JavaScript Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+                <img
+                  src={reactLogo}
+                  alt="React JS Logo"
+                  className="w-12 h-12 border-2 border-secondary-500 p-1 rounded-xl"
+                ></img>
+              </div>
               <div className="button-group flex space-x-8 md:space-x-32 mt-2">
                 <a
                   href="https://todo-app-lime-two.vercel.app/"
@@ -256,14 +380,14 @@ function App() {
                   href="https://github.com/EddOliver24/todo-app"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110"
+                  className="px-4 py-2 rounded text-sm lg:text-xl border-2 border-secondary-100 transform transition-transform duration-300 hover:scale-110 bg-secondary-500"
                 >
                   View Code
                 </a>
               </div>
             </section>
           </div>
-          <div className="partition" id="questions"></div>
+          <div className="partition bg-secondary-300" id="questions"></div>
         </section>
         <section>
           <div className="questions">
--- edd-portfolio/tailwind.config.js
+++ edd-portfolio/tailwind.config.js
@@ -7,7 +7,10 @@ export default {
         primary: "#FEFEFE",
         secondary: {
           100: "#F14902",
-          200: "#FECB00"
+          200: "#FECB00",
+          300: "#F6E7D8",
+          400: "#E13F2A",
+          500: "#303030"
         },
         tertiary: "#0D0D0B"
       },
